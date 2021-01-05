0b44c50c179eff6bf455f14d0f72499a70c76efa
colceriutudor <colceriu.tudor@yahoo.com>
2021-01-05 19:28:40 +0200

    final code (to be tested) + code comments

--- code_parking_sensor.ino
+++ code_parking_sensor.ino
@@ -1,35 +1,58 @@
+/*
+ * Code for the master board - Arduino UNO
+*/
+
 #include <SoftwareSerial.h>
 
+/*
+ * serial pins for the communication between the boards
+*/
 SoftwareSerial serialPins(5, 6);
 
+/*
+ * variables used for the board pins
+*/
 const int BUZZER_PIN = 8;
 const int TRIG_PIN = 9;
 const int ECHO_PIN = 10;
 
+/*
+ * control variables
+*/
 long duration;
-
-int distance, ok;
-int reservedParkingSpot_wifi, vehicleParked_wifi;
-
+int distance, timer_started;
+int reservedParkingSpot_wifi, vehicleParked_wifi, correctClient_wifi;
 uint32_t oldtime;
-
 bool vehicleParked;
 
+/*
+ * set the pins for the distance sensor module
+ */
 void distanceSensorSetup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 }
 
+/*
+ * set the pins for the buzzer module
+ */
 void buzzerSetup() {
   pinMode (BUZZER_PIN, OUTPUT) ;
 }
 
+/*
+ * initial state of the control variables
+ */
 void initVariables() {
   vehicleParked = false;
   vehicleParked_wifi = false;
   reservedParkingSpot_wifi = false;
 }
 
+/*
+ * setup function for all the components
+ * including the serial pins and serial monnitor
+ */
 void setup ()
 {
   initVariables();
@@ -42,12 +65,15 @@ void setup ()
   Serial.begin(9600);
 }
 
+/*
+ * the function for the buzzer module, making an alarm noise
+ */
 void alarm() {
-  unsigned char i, j ;
+  unsigned char i;
 
-  for (i = 0; i < 80; i++) // When a frequency sound
+  for (i = 0; i < 80; i++) //freq sound
   {
-    digitalWrite (BUZZER_PIN, HIGH) ; //send tone
+    digitalWrite (BUZZER_PIN, HIGH) ; //send a tone
     delay (1) ;
     digitalWrite (BUZZER_PIN, LOW) ; //no tone
     delay (1) ;
@@ -61,6 +87,12 @@ void alarm() {
   }
 }
 
+/*
+ * initial state of the distance sensor pins
+ * as the doccumentation states, the trig pin must be
+ * assigned a LOW / HIGH / LOW sequence in order to 
+ * initialize the distance sensor and have it functional
+ */
 void initDistanceSensor() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
@@ -69,66 +101,150 @@ void initDistanceSensor() {
   digitalWrite(TRIG_PIN, LOW);
 }
 
+/*
+ * function that calculates the distance given from the
+ * sensor and returns it in
+ * UNIT : cm
+ */
 float getDistance() {
+  
+  //initialize the sensor
   initDistanceSensor();
-  duration = pulseIn(ECHO_PIN, HIGH); //travel time in ms
+
+  //get the travel time in ms
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  //convert the travel time from ms to cm
   return (duration * 0.034 / 2);
 }
 
+/*
+ * function that sends the parking status (if vehicle is parked or not)
+ * to the wifi esp module using the serial communication
+ */
+void writeParkingStatus() {
+  serialPins.write((int)vehicleParked);
+}
+
+/*
+ * function that reads from the serial pins all the sent information
+ * from the wifi module
+ */
 void readSerialData() {
+
+  //check if any information is being recieved via serial pins
   if (serialPins.available() > 0) {
     reservedParkingSpot_wifi = serialPins.read();
     vehicleParked_wifi = serialPins.read();
+    correctClient_wifi = serialPins.read();
   }
+
+  Serial.println("reserved : " + reservedParkingSpot_wifi);
+  Serial.println("confirmed parking : " + reservedParkingSpot_wifi);
+  Serial.println("correct client confirmation : " + reservedParkingSpot_wifi);
+
 }
 
+/*
+ * function that begins the timer countdown
+ */
 void count() {
-  if (ok == 0) {
+
+  //if the timer hasn't started yet, start it
+  if (timer_started == 0) {
     oldtime = millis();
-    ok = 1;
+    timer_started = 1;
   }
 }
 
+/*
+ * main loop function
+ */
 void loop ()
 {
+  //get the distance from the sensor
   float distance = getDistance();
 
+  //a vehicle is considered as parked only when the sensor
+  //detects that the distance between the two is less than 10cm
   if (distance < 10) {
+
+    //change the status to parked (will also be sent via serial)
     vehicleParked = true;
+
+    //start the timer
     count();
   }
+  //the vehicle left the parking spot
   else if (distance >= 10) {
-    ok = 0;
+
+    //stop the count
+    timer_started = 0;
+
+    //change the status to empty space
     vehicleParked = false;
+
+    //stop the alarm if any
     digitalWrite (BUZZER_PIN, LOW) ;
   }
 
-  while (vehicleParked == true) {
+  //vehicle hasn't left the parking spot yet
+  while (vehicleParked) {
 
+    //get the information from the wifi module
     readSerialData();
 
-    if (reservedParkingSpot_wifi == true) {
+    //and notify that a vehicle is here now
+    writeParkingStatus();
+
+    //if the wrong vehicle parked (doesn't match the reservation)
+    if (!correctClient_wifi) {
+
+      //alarm the driver (cannot park in a reserved space)
       alarm();
+
+      //monnitor to see if the vehicle wants to leave
       distance = getDistance();
     }
     else {
-      Serial.println((millis() - oldtime) / 1000);
 
-      if ((millis() - oldtime) / 1000 > 60) {
+      //if the parking spot is indeed reserved
+      //and a certain driver stops there
+      if (reservedParkingSpot_wifi) {
+
+        //alarm the driver (cannot park in a reserved space)
         alarm();
+  
+        //monnitor to see if the vehicle wants to leave
+        distance = getDistance();
+      }
+      //if the parking spot is not taken / reserved and someone parks
+      else {
+
+        //give them a 1 minute cooldown to hit the "park" button
+        if ((millis() - oldtime) / 1000 > 60)
+          alarm();
+
+        //monnitor to see if the vehicle wants to leave
+        distance = getDistance();
       }
 
-      distance = getDistance();
-    }
+      //if, at any time, the vehicle leaves the parking spot, or they hit the "park" button
+      //and the spot is not reserved, stop the alarm
+      if (distance >= 10 || (vehicleParked_wifi && !reservedParkingSpot_wifi && correctClient_wifi)) {
 
-    if (distance >= 10 || (vehicleParked_wifi == true && reservedParkingSpot_wifi == false)) {
-      oldtime = millis();
-      break;
+        //reset the timer
+        oldtime = millis();
+
+        //send to the module that someone has parked (correctly)
+        writeParkingStatus();
+        
+        break;
+      }
     }
   }
 
-  Serial.println("OUT");
-
+  //update the control varialbes provided by the wifi module
   readSerialData();
 
   delay(1000);
--- code_parking_sensor_WIFI_module/code_parking_sensor_WIFI_module.ino
+++ code_parking_sensor_WIFI_module/code_parking_sensor_WIFI_module.ino
@@ -1,44 +1,67 @@
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 
-//serial pins for the communication between the boards
+/*
+ * serial pins for the communication between the boards
+ */
 SoftwareSerial serialPins(D6, D5);
 
 const char* ssid = "";
 const char* password = "";
 
-//server created by the wifi module
+/*
+ * server created by the wifi module
+ */
 WiFiServer server(80);
 
-//client connected to the server
+/*
+ * client connected to the server
+ */
 WiFiClient client;
 
-//information recieved from the uno board
-//via serial communication
+/*
+ * information recieved from the uno board
+ * via serial communication
+ */
 int recievedData;
 
-//control variables
+/*
+ * control variables
+*/
+bool isCorrectParkedClient;
 bool reserved, isVehicleParked;
+String reservedClientID;
 
-void setup() {
-  //initial state for the control variables (false)
+
+/*
+ *  assume there can only be one
+ *  client connected to the network
+ *  performing operations
+*/
+String connectedClientID;
+
+/*
+ * set the initial state for the control variables (false)
+ */
+void initializeVariables(){
   reserved = false;
   isVehicleParked = false;
+  isCorrectParkedClient = true;
+  reservedClientID = "";
+}
 
-  //init and set the baudrate for the serial monnitor
-  Serial.begin(115200);
-  delay(10);
-
-  //init and set the baudrate for the sw serial pins
-  serialPins.begin(9600);
-  delay(10);
-
+/*
+ * function that initializes the wifi server
+ * with the appropriate credentials
+ * and also prints the IP address for the users to connect to
+ */
+void startServer(){
   //begin a wifi connection using the provided credentials
   WiFi.begin(ssid, password);
 
   //set up the soft access point in order to connect
   //to the page (via provided IP)
-  WiFi.softAP("Homikes","sotapssidsecure");
+  WiFi.softAP("Homikes", "sotapssidsecure");
 
   //wait for a connection to be established
   while (WiFi.status() != WL_CONNECTED) {
@@ -49,41 +72,85 @@ void setup() {
   //after the connection is made start the server
   server.begin();
   Serial.println("server on");
-  
+
   //and print the IP for the user to connect to
   Serial.println(WiFi.localIP());
 }
 
-//get the requests via user input from the server
-//and parse them accordingly
-void parseServerRequest(String request){
+/*
+ * main setup function for the components
+ */
+void setup() {
+  initializeVariables();
+
+  //init and set the baudrate for the serial monnitor
+  Serial.begin(115200);
+  delay(10);
+
+  //init and set the baudrate for the sw serial pins
+  serialPins.begin(9600);
+  delay(10);
+
+  //start the server
+  startServer();
+}
+
+/*
+ * function that gets the requests via user input from the server
+ * and parses them accordingly
+ */
+void parseServerRequest(String request) {
+
   //found "RESERVED" in the request String
   if (request.indexOf("/RESERVED") != -1)  {
+
     //if the reserve button is pushed
     //then reserve the parking space
     reserved = true;
+
+    //and mark the client that reserved it
+    reservedClientID = getClientStatus();
+
+    Serial.println(reservedClientID + " reserved the spot");
   }
+
   //found "PARK" in the request String
   if (request.indexOf("/PARK") != -1)  {
-    //if a person just parked set the
-    //variable to true
-    isVehicleParked = true;
 
-    //if a vehicle is parked, the parking space
-    //is no longer reserved
-    reserved = false;
+    //see what client wants to park
+    connectedClientID = getClientStatus();
+
+    Serial.println(connectedClientID + " wants to park");
+    Serial.println(reservedClientID + " booked");
+
+    //check if the parked client is the correct
+    //one (the one that reserved the parking space)
+    if (reservedClientID.equals(connectedClientID)) {
+
+      //if a vehicle is parked, the parking space
+      //is no longer reserved
+      reserved = false;
+
+      //if a person just parked set the
+      //variable to true
+      isVehicleParked = true;
+    }
+    //else notify the system
+    else
+      notifyParkingStatus();
   }
 }
 
-//after client feedback, change the server
-//appearance accordingly, using HTML
-void respond(){
+/*
+ * function that changes the server accordingly (via HTML)
+ * after client feedback
+ */
+void respond() {
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("");
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
-
   client.print("Parking space is ");
 
   //display the state of the parking space
@@ -100,16 +167,60 @@ void respond(){
   client.println("</html>");
 }
 
-//send the parking space status (vehicle parked or not)
-//to the main board in order to be parsed accordingly
-//using the serial communication between the two boards
-void notifyParkingStatus(){
+/*
+ * function that sends the parking space status (vehicle parked or not)
+ * to the main board in order to be parsed accordingly
+ * using the serial communication between the two boards
+ */
+void notifyParkingStatus() {
   serialPins.write((int)reserved);
   serialPins.write((int)isVehicleParked);
+  serialPins.write((int)isCorrectParkedClient);
 }
 
-//main loop for this module (ESP8266WiFi)
+/*
+ * functions that revieves the parking space status
+ * from the main board via serial pins
+ */
+void readParkingStatus() {
+
+  //if a vehicle is not parked anymore, update the infromation
+  //stored in the control variables
+  if (serialPins.available() > 0 && serialPins.read() == 0)
+    isVehicleParked = false;
+}
+
+/*
+ * function that returns the client address
+ * computes the client MAC address and converts it to String
+ * in order to be further used
+ */
+String getClientStatus() {
+  String clientAddress = "";
+
+  struct station_info *stat_info;
+  struct ip4_addr *IPaddress;
+
+  stat_info = wifi_softap_get_station_info();
+
+  if (stat_info != NULL) {    
+    clientAddress =
+      String(stat_info->bssid[0]) +
+      String(stat_info->bssid[1]) +
+      String(stat_info->bssid[2]) +
+      String(stat_info->bssid[3]) +
+      String(stat_info->bssid[4]) +
+      String(stat_info->bssid[5]);
+  }
+
+  return clientAddress;
+}
+
+/*
+ * main loop for this module (ESP8266WiFi)
+ */
 void loop() {
+  
   //get the client status (client found or not)
   client = server.available();
   if (!client) {
@@ -130,6 +241,8 @@ void loop() {
   //client and not yet read
   client.flush();
 
+  readParkingStatus();
+
   //after the request is made by the client
   //send it to be parsed
   parseServerRequest(request);
@@ -141,6 +254,6 @@ void loop() {
   //also signal to the main board using
   //the serial communication
   notifyParkingStatus();
-  
+
   delay(1);
 }
